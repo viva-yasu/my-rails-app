ec91a3c0972757dd30876eaf037adb4960bda208
Yasunari Kondo <kyasunari123v@gmail.com>
2015-09-13 04:58:07 +0900

    routes action template assign

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,4 @@
 class Image < ActiveRecord::Base
   validates :url, presence: true
+  validates :title, presence: true
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe Image, type: :model do
         expect(no_url_image.errors[:url]).to_not be_empty
       end
     end
-  end
+    describe "title" do
+      let(:no_title_image) { Image.new(url: nil, title: nil) }
+
+      it "require url" do
+        expect(no_title_image).to_not be_valid
+        expect(no_title_image.errors[:title]).to_not be_empty
+      end
+    end
+ end
 end
